b55902999bdf3f73f3933c4c014a360fb875344c
Danil Panarin <danilpanarin@thefrontends-MacBook-Air.local>
2023-05-25 02:45:35 +0600

    [initial]

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import NavBar from './components/navbar/Navbar'
+import './globals.css'
+import { Nunito } from 'next/font/google'
+
+const font = Nunito({ subsets: ['latin'] })
+
+export const metadata = {
+  title: '✈️ Vacation Homes &amp; Condo Rentals - Airbnb',
+  description: 'Find the perfect place to stay at an amazing price in 191 countries. Belong anywhere with Airbnb.',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <NavBar />
+        {children}
+      </body>
+    </html>
+  )
+}

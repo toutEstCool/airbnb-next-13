c12305d8ab2471d07b6174b0b4f32c851aff8d3d
Danil Panarin <danilpanarin@thefrontends-MacBook-Air.local>
2023-06-08 15:32:06 +0600

    register-modal

---
+++ app/components/modals/registermodal/RegisterModal.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import axiso from 'axios';
+import { AiFillGithub } from 'react-icons/ai'
+import { FcGoogle } from 'react-icons/fc'
+import { useCallback, useState } from 'react';
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+import useRegisterModal from '@/app/hooks/useRegisterModal';
+
+const RegisterModal = (): JSX.Element => {
+  return (
+    <h1>register modal</h1>
+  )
+}
+
+export default RegisterModal;
